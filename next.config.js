349f19f4e96d38d3c9ef72fd2cef1c21bad4419d
JangSeBaRi <jangsj0325@gmail.com>
2023-04-25 09:40:55 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: [
+      'https://next13-starbucks-hompage-ajw9r3qzg-jangsebari.vercel.app/',
+    ]
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'www.starbucks.co.kr',
+    //     port: '',
+    //     // pathname: '',
+    //   },
+    // ],
+  },
 }
 
 module.exports = nextConfig
