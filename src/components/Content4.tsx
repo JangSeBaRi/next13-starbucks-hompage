58b271f04cc9bc39daa94786e86b6044b10ef7fa
JangSeBaRi <jangsj0325@gmail.com>
2023-04-27 12:22:15 +0900

    commit

--- src/components/Content4.tsx
+++ src/components/Content4.tsx
@@ -2,13 +2,43 @@
 
 import { windowInnerWidthRecoil } from "@/recoil/states";
 import Image from "next/image";
+import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 
 const Content4 = () => {
     const windowInnerWidth = useRecoilValue(windowInnerWidthRecoil);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const html = document.querySelector("html")!!;
+            const content4El = document.querySelector("#content_4") as HTMLDivElement;
+            const animation1El = document.querySelector("#animation_1") as HTMLDivElement;
+            const animation2El = document.querySelector("#animation_2") as HTMLDivElement;
+            if (content4El.offsetTop + content4El.offsetHeight / 3 < html.clientHeight + html.scrollTop) {
+                animation1El.style.opacity = "1";
+                animation2El.style.opacity = "1";
+                if (windowInnerWidth > 960) {
+                    animation1El.style.left = "5%";
+                    animation2El.style.right = "12%";
+                }
+            } else {
+                animation1El.style.opacity = "0";
+                animation2El.style.opacity = "0";
+                if (windowInnerWidth > 960) {
+                    animation1El.style.left = "-70%";
+                    animation2El.style.right = "-60%";
+                }
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [windowInnerWidth]);
+
     return (
         <div
+            id="content_4"
             className="bg-cover bg-center"
             style={{
                 height:
@@ -27,12 +57,14 @@ const Content4 = () => {
         >
             <div className="relative max-w-[1124px] mx-auto h-full">
                 <div
-                    className="absolute"
-                    style={
+                    id="animation_1"
+                    className="absolute opacity-0"
+                    style={Object.assign(
+                        { transition: "opacity 3s, left 3s" },
                         windowInnerWidth > 960
-                            ? { left: "5%", top: "50%", width: "34%", transform: "translateY(-50%)" }
+                            ? { left: "-70%", top: "50%", width: "34%", transform: "translateY(-50%)" }
                             : { left: "50%", top: "4%", transform: "translateX(-50%)", width: "63%" }
-                    }
+                    )}
                 >
                     {windowInnerWidth > 960 ? (
                         <Image
@@ -57,12 +89,14 @@ const Content4 = () => {
                     )}
                 </div>
                 <div
-                    className="absolute"
-                    style={
+                    id="animation_2"
+                    className="absolute opacity-0"
+                    style={Object.assign(
+                        { transition: "opacity 3s, right 3s" },
                         windowInnerWidth > 960
-                            ? { right: "12%", top: "32.8%" }
+                            ? { right: "-60%", top: "32.8%" }
                             : { right: "50%", top: "60%", bottom: "10.5%", transform: "translateX(50%)", width: "62%" }
-                    }
+                    )}
                 >
                     {windowInnerWidth > 960 ? (
                         <Image
